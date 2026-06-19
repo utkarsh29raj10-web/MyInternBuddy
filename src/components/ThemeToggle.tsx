5312fb01cab644e4e40170697414557c21cd68fc
Utkarsh Raj <utkarsh@TRIZ.local>
2026-06-19 13:42:43 +0530

    Setup variables for project-wide use, Created & tested theme toggler, Updated readme

---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import {useState, useEffect} from "react";
+import {Sun, Moon} from "lucide-react";
+
+export default function ThemeToggle() {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    useEffect(() => {
+        setIsDarkMode(document.documentElement.classList.contains("dark"));
+    }, []);
+
+    const toggleTheme = () => {
+        setIsDarkMode((prev) => {
+            const nextMode = !prev;
+            if (nextMode) {
+                document.documentElement.classList.add("dark");
+                document.documentElement.classList.remove("light");
+            } else {
+                document.documentElement.classList.remove("dark");
+                document.documentElement.classList.add("light");
+            }
+
+            return nextMode;
+        });
+    };
+
+    return (
+        <button
+            type="button"
+            onClick={toggleTheme}
+            className="p-2 text-primary rounded-md m-4 transition-colors hover:opacity-80 flex active:opacity-50 items-center justify-center"
+                aria-label="Toggle Theme"
+        >
+            {isDarkMode ? <Sun size="var(--text-xl)"/> : <Moon size="var(--text-xl)"/>}
+        </button>
+    );
+}
---
+++ src/constants/config.ts
@@ -0,0 +1,4 @@
+export const SITE_CONFIG = {
+    brandName: "MyInternBuddy",
+    url: process.env.NEXT_PUBLIC_URL || "http://localhost:3000/",
+}
