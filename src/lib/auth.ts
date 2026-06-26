652dd538424e5dc6fa7c88bcf0d8d71153607e2c
Utkarsh Raj <utkarsh29raj10@gmail.com>
2026-06-26 06:30:14 +0530

    Updated database to support google authentication and updated auth.ts to allow google sign in when sign up was via the same email

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -12,6 +12,7 @@ export const authOptions: NextAuthOptions = {
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID || "",
             clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+            allowDangerousEmailAccountLinking: true,
         }),
         CredentialsProvider({
             name: "Credentials",
