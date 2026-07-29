968ce8d4a08a6febb0f5a8f2a4bacf4ec30367d2
Utkarsh Raj <utkarsh29raj10@gmail.com>
2026-07-29 22:07:23 +0530

    Built POST & DELETE API routes for saving internships

---
+++ src/app/api/internships/save/route.ts
@@ -0,0 +1,91 @@
+import {NextRequest, NextResponse} from "next/server";
+import {getServerSession} from "next-auth/next";
+import {authOptions} from "@/lib/auth";
+import {prisma} from "@/lib/prisma";
+
+export async function POST(req: NextRequest) {
+    try {
+        const session = await getServerSession(authOptions);
+
+        if(!session || !session.user) {
+            return NextResponse.json(
+                {success: false, error: "Unauthorized"},
+                {status: 401}
+            );
+        }
+
+        const body = await req.json();
+        const {internshipId} = body;
+
+        if (!internshipId) {
+          return NextResponse.json(
+              {success: false, error: "Internship ID is required"},
+              {status: 400}
+          );
+        }
+
+        const savedInternship = await prisma.savedInternship.create({
+            data: {
+                userId: session.user.id,
+                internshipId: internshipId
+            }
+        });
+
+        return NextResponse.json({success: true, data: savedInternship});
+    }
+    catch (error: any) {
+        if (error.code === "P2002") {
+            return NextResponse.json(
+                {success: false, error: "Already Saved"},
+                {status: 400}
+            );
+        }
+        console.error("Error saving internship", error);
+        return NextResponse.json(
+            {success: false, error: "Internal Server Error"},
+            {status: 500}
+        );
+    }
+}
+
+export async function DELETE(req: NextRequest) {
+    try {
+        const session = await getServerSession(authOptions);
+        if (!session || !session.user) {
+            return NextResponse.json(
+                {success: false, error: "Unauthorized"},
+                {status: 401}
+            );
+        }
+
+        const searchParams = req.nextUrl.searchParams;
+        const internshipId = searchParams.get("internshipId");
+
+        if (!internshipId) {
+            return NextResponse.json(
+                {success: false, error: "Internship ID is required."},
+                {status: 400}
+            );
+        }
+
+        await prisma.savedInternship.delete({
+            where: {
+                userId_internshipId: {
+                    userId: session.user.id,
+                    internshipId: internshipId
+                }
+            }
+        });
+
+        return NextResponse.json(
+            {success: true, message: "Internship unsaved"}
+        );
+    }
+    catch (error) {
+        console.error("Error unsaving internship", error);
+        return NextResponse.json(
+            {success: true, error: "Internal Server Error"},
+            {status: 500}
+        );
+    }
+}
