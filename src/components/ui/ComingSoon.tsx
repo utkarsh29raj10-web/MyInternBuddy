b8175b23094abb0ac241e7113ac5bc031833cdef
Utkarsh Raj <utkarsh29raj10@gmail.com>
2026-08-23 12:07:06 +0530

    Changed transparency color

--- src/components/ui/ComingSoon.tsx
+++ src/components/ui/ComingSoon.tsx
@@ -9,7 +9,7 @@ export function ComingSoon(
     }) {
     return (
         <div className="w-full h-min flex items-center justify-center px-6 animate-fade-in">
-            <div className="max-w-md w-full bg-gradient-to-br from-secondary to-background p-[1px] rounded-3xl shadow-2xl relative overflow-hidden group">
+            <div className="max-w-md w-full bg-gradient-to-br from-blue-400/80 to-background p-[1px] rounded-3xl shadow-2xl relative overflow-hidden group">
                 <div className="w-full h-full bg-background/70 backdrop-blur-xl p-10 rounded-3xl flex flex-col items-center text-center gap-4 relative z-10">
                     <div className="relative mt-2">
                         <div className="absolute inset-0 bg-primary/20 blur-xl rounded-full animate-pulse"/>
