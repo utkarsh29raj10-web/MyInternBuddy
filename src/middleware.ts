929a97c4ee1b24c16e32ad4a1d04257a38fa9ad0
Utkarsh Raj <utkarsh29raj10@gmail.com>
2026-08-24 06:58:54 +0530

    Updated route to send users to onboarding if their onboarded flag shows they haven't onboarded yet

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,16 @@ import {NextResponse} from "next/server";
 
 export default withAuth(
     function middleware(req) {
+        const token = req.nextauth.token;
+        const isNotOnboarded = token && token.onboarded === false;
+        const isOnboardingRoute = req.nextUrl.pathname.startsWith("/onboarding");
+
+        if (isNotOnboarded && !isOnboardingRoute)
+            return NextResponse.redirect(new URL("/onboarding", req.url));
+
+        if (!isNotOnboarded && isOnboardingRoute)
+            return NextResponse.redirect(new URL("/profile", req.url));
+
         return NextResponse.next();
     },
     {
